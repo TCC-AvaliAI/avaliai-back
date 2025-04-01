9d32572e4bd2b9ac57f35d305b214bc3d3252560
todpig <luccasraffael6@gmail.com>
2025-04-01 14:42:46 -0300

    feat: add and list question by exam id

--- apps/exam/views.py
+++ apps/exam/views.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 from .models import Exam
 from .serializers import ExamSerializer
+from apps.question.serializers import QuestionSerializer
 
 class ExamListAndCreate(APIView):
     def get(self, request):
@@ -31,3 +32,18 @@ class ExamUpdateAndDelete(APIView):
         exam = get_object_or_404(Exam, pk=exam_id)
         exam.delete()
         return Response({"message": "Exam deleted"}, status=status.HTTP_204_NO_CONTENT)
+    
+class ExamQuestions(APIView):
+    def get(self, request, exam_id):
+        exam = get_object_or_404(Exam, pk=exam_id)
+        questions = exam.questions.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request, exam_id):
+        exam = get_object_or_404(Exam, pk=exam_id)
+        question_id = request.data.get('question_id')
+        if not question_id:
+            return Response({"error": "question_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+        exam.questions.add(question_id)
+        return Response({"message": "Question added to exam"}, status=status.HTTP_200_OK)
