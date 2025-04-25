649b35fe5da3543c683d6ee710f6f9f8636426fd
todpig <luccasraffael6@gmail.com>
2025-04-24 22:01:57 -0300

    feat: update serializer

--- apps/exam/serializers.py
+++ apps/exam/serializers.py
@@ -1,26 +1,42 @@
 from rest_framework import serializers
 from .models import Exam
-from apps.question.models import Question
+from apps.question.models import QuestionType
 from apps.question.serializers import QuestionSerializer
 import uuid
 
-class SimpleQuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        exclude = ('user',)
+class NestedQuestionSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=500)
+    options = serializers.ListField(child=serializers.CharField(max_length=100), required=False)
+    answer = serializers.IntegerField(required=False)
+    answer_text = serializers.CharField(required=False)
+    type = serializers.ChoiceField(choices=QuestionType.choices)
+    score = serializers.IntegerField(default=0)
 
 class ExamSerializer(serializers.ModelSerializer):
     discipline_name = serializers.CharField(source='discipline.name', read_only=True)
     classroom_name = serializers.CharField(source='classroom.name', read_only=True)
-    questions = SimpleQuestionSerializer(many=True, read_only=True)
+    questions = NestedQuestionSerializer(many=True, required=False)
 
     class Meta:
         model = Exam
         exclude = ('user',)
 
     def create(self, validated_data):
+        questions_data = validated_data.pop('questions', None)
         validated_data['user'] = self.context['request'].user
-        return super().create(validated_data)
+        exam = Exam.objects.create(**validated_data)
+        
+        if questions_data:
+            from apps.question.models import Question
+            for question_data in questions_data:
+                # Adiciona o usuário atual aos dados da questão
+                question = Question.objects.create(
+                    user=self.context['request'].user,
+                    **question_data
+                )
+                exam.questions.add(question)
+        
+        return exam
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
